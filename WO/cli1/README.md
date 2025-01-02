85efd8450a16721a9b47ba3d7a4e6376e7f0db22
kachraz <110726496+kachraz@users.noreply.github.com>
2025-01-02 02:15:47 +0000

    🫦

---
+++ WO/cli1/README.md
@@ -0,0 +1,7 @@
+
+1. [CLi1](#cli1)
+
+
+# CLi1
+
+1. First version of the cli app for the project
---
+++ WO/cli1/panty.py
@@ -0,0 +1,22 @@
+# CLI App 1 with tpper
+from rich import print as rprint
+
+
+def add_user():
+    rprint("Add user")
+
+
+def delete_user():
+    rprint("Delete user")
+
+
+def update_user():
+    rprint("Update user")
+
+
+def main():
+    rprint("Hello from cli1!")
+
+
+if __name__ == "__main__":
+    main()
